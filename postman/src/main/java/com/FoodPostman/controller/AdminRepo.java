e8f9b7bb03660ff8e0ba268f3417b203b91aab72
Shivaamoria <vaibhav.jain@walkingtree.com>
2022-07-07 14:32:09 +0530

    awasthi

---
+++ postman/src/main/java/com/FoodPostman/controller/AdminRepo.java
@@ -0,0 +1,15 @@
+package com.FoodPostman.controller;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface AdminRepo extends JpaRepository<Admin, String> {
+
+
+
+
+	public List<Admin> findByEmailAndPassword(String email, String password);
+}
+
